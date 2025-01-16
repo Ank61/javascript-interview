f1066c9e1ccaf30be76a5ac829b72a0405b9d4c8
Ankit Chohan <ankitchohan61@gmail.com>
2025-01-16 19:58:54 +0530

    Added new changes

---
+++ dsa/array/index.js
@@ -0,0 +1,27 @@
+//Problem :
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+var canPlaceFlowers = function (flowerbed, n) {
+  let flowers = n;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      flowers--;
+
+      if (flowers === 0) {
+        return true;
+      }
+    }
+  }
+  return flowers <= 0;
+};
