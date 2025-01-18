904259e6ddee455168b1cab17f209e581f75ef2d
Ankit Chohan <ankitchohan61@gmail.com>
2025-01-18 21:45:11 +0530

    added new changes

--- dsa/array/index.js
+++ dsa/array/index.js
@@ -58,3 +58,21 @@ var canPlaceFlowers = function (flowerbed, n) {
 
 // const reverse = kidsWithCandies([2, 3, 5, 1, 3], 3);
 // console.log(reverse)
+
+// var moveZeroes = function (nums) {
+//   let nonZeroIndex = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] !== 0) {
+//           nums[nonZeroIndex] = nums[i];
+//           nonZeroIndex++;
+//       }
+//   }
+// console.log()
+//   for (let i = nonZeroIndex; i < nums.length; i++) {
+//       nums[i] = 0;
+//   }
+//   return nums
+// };
+// const reverse = moveZeroes([0,1,0,3,12]);
+// console.log(reverse)
